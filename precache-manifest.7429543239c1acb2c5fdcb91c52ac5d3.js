3b3d6836bfd561749011e7e7b3e825ac8df96ad9
balfron <40870085+balfron@users.noreply.github.com>
2020-10-19 20:16:16 +0100

    Updates

---
+++ precache-manifest.7429543239c1acb2c5fdcb91c52ac5d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7789b0ed7223abc0b71eea652d52621c",
+    "url": "/oni/index.html"
+  },
+  {
+    "revision": "3e25eeee420a7b4f6da4",
+    "url": "/oni/static/css/main.757e3137.chunk.css"
+  },
+  {
+    "revision": "def59ab47ba98ac793f8",
+    "url": "/oni/static/js/2.cf330350.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/oni/static/js/2.cf330350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e25eeee420a7b4f6da4",
+    "url": "/oni/static/js/main.a9706b7a.chunk.js"
+  },
+  {
+    "revision": "b69b3ac3361d7b7ec29e",
+    "url": "/oni/static/js/runtime-main.e10ad56d.js"
+  },
+  {
+    "revision": "d2eb8d92b51ef116d1314631d92c8ecc",
+    "url": "/oni/static/media/oni.d2eb8d92.svg"
+  }
+]);
